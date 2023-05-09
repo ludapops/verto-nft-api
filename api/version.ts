2d5f00e6e96ea90e43ba11bd2b8064698ff1fcb1
marcmathieu <marc@codeparticle.com>
2023-05-09 11:15:40 -0400

    feat: Remove vercel files

--- api/version.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { version } from "../package.json";
-
-export default (req: VercelRequest, res: VercelResponse): void => {
-  res.json({ version });
-};
